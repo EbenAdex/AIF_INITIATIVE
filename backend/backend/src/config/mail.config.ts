50349b97c719bd4d4d6c3666e5b807b1a01554d0
Akorede Ajibola <ajibolaakorede2021@gmail.com>
2026-07-27 02:43:47 +0100

    fix: smtp email sending

--- backend/backend/src/config/mail.config.ts
+++ backend/backend/src/config/mail.config.ts
@@ -3,6 +3,7 @@ export const mailConfig = {
     host: process.env.MAIL_HOST,
     port: Number(process.env.MAIL_PORT),
     secure: false,
+    family: 4,
     auth: {
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASSWORD,
